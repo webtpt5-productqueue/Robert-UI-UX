1fba70c4ae16bcfc450041dbc31edb681e30a3ea
Robert Pedersen <rob84044@gmail.com>
2019-04-30 15:16:10 -0600

    Completed the code for the Github event listener. I tried to add the the code for the portfolio page event listener but it was not wanting to work for me. I also need to style the two spots where there will be a compoonents for js in the landing page and the about page

--- JS/github.js
+++ JS/github.js
@@ -3,12 +3,22 @@ toggle between hiding and showing the dropdown content */
 const gitHub = document.querySelector("#github")
 const li = document.querySelectorAll(".footer-link")
 
+
+
 // Close the dropdown menu if the user clicks outside of it
 
-
-li[2].addEventListener('mouseenter', function(){
-  gitHub.classList.remove("hidden")
+li[2].addEventListener('click', function(){
+   gitHub.classList.remove("hidden")
   let hideMe = document.querySelector('.dropbtn')
-  let addPadding = document.querySelector('')
-  hideMe.style.innerHTML = "none";
+  let addPadding = document.querySelectorAll('.github a');
+  hideMe.innerHTML = "";
+});
+
+gitHub.addEventListener('mouseout', function(){
+  
+  setTimeout(function(){
+  gitHub.classList.add("hidden")
+  let hideMe = document.querySelector('.dropbtn')
+  hideMe.innerHTML = "Github";
+  }, 7000)
 });
