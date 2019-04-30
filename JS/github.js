5c8a810bc874257413ce8393732e05e50bd7cb64
Robert Pedersen <rob84044@users.noreply.github.com>
2019-04-29 19:58:57 -0600

    All files added to repo

---
+++ JS/github.js
@@ -0,0 +1,28 @@
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function gitHub() {
+  document.querySelector("#github-socialMedia").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+
+gitHub.addEventListener('click', function(e){
+  if(!e.target.matches('.dropbtn')){
+    let dropdowns = document.querySelector('.dropdown-gituhub')
+    if(dropdowns.classList.contains('show')){
+      dropdowns.classList.remove('show');
+    }
+  }
+})
+// window.onclick = function(event) {
+//   if (!event.target.matches('.dropbtn')) {
+//     var dropdowns = document.getElementsByClassName("dropdown-content");
+//     var i;
+//     for (i = 0; i < dropdowns.length; i++) {
+//       var openDropdown = dropdowns[i];
+//       if (openDropdown.classList.contains('show')) {
+//         openDropdown.classList.remove('show');
+//       }
+//     }
+//   }
+// }
